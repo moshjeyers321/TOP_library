a2a13da3c72bcb60554034e77725914a51c4e390
Josh <pianoman04428@gmail.com>
2021-11-19 16:46:35 -0600

    added delete functionality for books in the bookshelf

--- app.js
+++ app.js
@@ -19,21 +19,21 @@ function Book(title, author, numPages, read) {
 }
 
 Book.prototype.info = function () {
-    return `${this.title} by ${this.author}, ${this.numPages} pages, ${this.read === true ? 'read' : 'not year read'}`;
+    return `${this.title} by ${this.author}, ${this.numPages} pages, ${this.read === true ? 'read' : 'not yet read'}`;
 }
 
 function getBookFromInput() {
     const title = document.getElementById('title').value
     const author = document.getElementById('author').value
     const pageCount = document.getElementById('pageCount').value
-    const isRead = document.getElementById('isRead').value
+    const isRead = document.getElementById('isRead').checked
     return new Book(title, author, pageCount, isRead);
 }
 
 function addBookToLibrary(book) {
     if (!isInLibrary(book)) {
-        myLibrary.push(book);
         addBookToDisplay(book);
+        myLibrary.push(book);
         // displayLibrary();
     } else {
         alert("Book Already Exists")
@@ -42,34 +42,38 @@ function addBookToLibrary(book) {
 
 function addBookToDisplay(bookInfo) {
     const book = document.createElement("div");
-    const displayTitle = document.createElement("div");
-    const displayAuthor = document.createElement("div");
-    const displayPageCount = document.createElement("div");
+    const title = document.createElement("div");
+    const author = document.createElement("div");
+    const pageCount = document.createElement("div");
     const bookBtns = document.createElement("div")
-    const displayRead = document.createElement("button");
-    const displayDelete = document.createElement("button");
+    const readBtn = document.createElement("button");
+    const deleteBtn = document.createElement("button");
 
     book.classList.add('book');
-    displayTitle.classList.add('bookTitle');
-    displayAuthor.classList.add('bookAuthor');
-    displayPageCount.classList.add('bookPageCount');
+    book.dataset.id = bookInfo.title;
+    title.classList.add('bookTitle');
+    author.classList.add('bookAuthor');
+    pageCount.classList.add('bookPageCount');
     bookBtns.classList.add('bookBtns');
-    displayRead.classList.add('bookReadBtn');
-    displayDelete.classList.add('bookDelBtn');
+    readBtn.classList.add('bookReadBtn', 'bookBtn');
+    deleteBtn.classList.add('bookDelBtn', 'bookBtn');
 
+    deleteBtn.addEventListener('click', () => {
+        removeBook(bookInfo.title);
+    })
 
-    displayTitle.textContent = bookInfo.title
-    displayAuthor.textContent = bookInfo.author
-    displayPageCount.textContent = `Page Count: ${bookInfo.numPages}`
-    displayRead.textContent = `${bookInfo.read === true ? "Read" : "Not Read"}`;
-    displayDelete.textContent = "Delete";
+    title.textContent = bookInfo.title
+    author.textContent = bookInfo.author
+    pageCount.textContent = `Page Count: ${bookInfo.numPages}`
+    readBtn.textContent = `${bookInfo.read === true ? "Read" : "Not Read"}`;
+    deleteBtn.textContent = "Delete";
 
-    bookBtns.appendChild(displayDelete);
-    bookBtns.appendChild(displayRead);
+    bookBtns.appendChild(readBtn);
+    bookBtns.appendChild(deleteBtn);
 
-    book.appendChild(displayTitle);
-    book.appendChild(displayAuthor);
-    book.appendChild(displayPageCount);
+    book.appendChild(title);
+    book.appendChild(author);
+    book.appendChild(pageCount);
     book.appendChild(bookBtns);
 
     bookShelf.appendChild(book);
@@ -79,6 +83,16 @@ function isInLibrary(newBook) {
     return myLibrary.some(book => book.title === newBook.title)
 }
 
+function removeBook(title) {
+    myLibrary = myLibrary.filter((book) => book.title !== title);
+    const bookToRemove = bookShelf.querySelector(`[data-id = ${CSS.escape(title)}]`)
+    console.log(bookToRemove)
+    bookShelf.removeChild(bookToRemove);
+}
+
+function toggleRead(book) {
+
+}
 
 const book1 = new Book("Harry Potter", 'JK Rowling', 534, true)
 const book2 = new Book("Cat in the Hat", 'Dr. Seuss', 42, true)
